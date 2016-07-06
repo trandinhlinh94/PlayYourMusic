cf26a1e0944423b129080bd7b5ac42fc19c7842f
trandinhlinh94 <trandinhlinh94@gmail.com>
2016-07-06 10:02:31 +0300

    add a property for HTTP connection

---
+++ PlayYourMusic/PlayYourMusic/SoundtrackCell.swift
@@ -0,0 +1,24 @@
+//
+//  SoundtrackCell.swift
+//  PlayYourMusic
+//
+//  Created by Linh Tran on 06/07/16.
+//  Copyright © 2016 Linh Tran. All rights reserved.
+//
+
+import UIKit
+
+class SoundtrackCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
