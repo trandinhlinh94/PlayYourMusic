295f16818a17cbca53db992acb49365f99b0ad10
trandinhlinh94 <trandinhlinh94@gmail.com>
2016-07-10 15:31:08 +0300

    add viewController for the searchView to send the searched term to the ListViewController then it will looked for the artist by itunes search API

--- PlayYourMusic/PlayYourMusic/SearchViewController.swift
+++ PlayYourMusic/PlayYourMusic/SearchViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var searchTextField: UITextField!
 
+    @IBOutlet weak var searchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchTextField.placeholder = "enter artist you want to search"
+        searchTextField.delegate = self
+        searchTextField.enablesReturnKeyAutomatically = true
+        searchButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +27,65 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK BUTTON "search" action
+    @IBAction func getSearchedArtistButtonTapped(sender: AnyObject) {
+        let text = searchTextField.text
+        if text!.isEmpty {
+            return
+        } else {
+            searchTextField.text = text!.urlEncoded
+            print(text!)
+        }
+    }
+    
+    // MARK UITextFieldDelegate and its methods
+    
+    // Enable the "Search" for your city above button
+    // if the search text field contains any text,
+    // disable it otherwise.
+    func textField(textField: UITextField,
+                   shouldChangeCharactersInRange range: NSRange,
+                                                 replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        let prospectiveText = (currentText as NSString).stringByReplacingCharactersInRange(
+            range,
+            withString: string)
+        searchButton.enabled = prospectiveText.characters.count > 0
+        print("Count: \(prospectiveText.characters.count)")
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        searchTextField.resignFirstResponder()
+        getSearchedArtistButtonTapped(searchButton)
+        return true
+    }
 
-    /*
+    
+    // Tapping on the view should dismiss the keyboard.
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if (segue.identifier == "listView") {
+            let text = searchTextField.text
+            let lvc = segue.destinationViewController as! ListViewController
+            lvc.searchedTerm = text!
+        }
     }
-    */
 
+    
+
+}
+extension String {
+    
+    // A method for %-encoding strings containing spaces and other
+    // characters that need to be converted for use in URLs.
+    var urlEncoded: String {
+        return self.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLUserAllowedCharacterSet())!
+    }
+    
 }
